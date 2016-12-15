d1dc67da550ced5da4147ef4c3af1394402796e7
Klaster_1 <Klaster1@gmail.com>
2016-12-15 19:42:43 +0700

    search

--- src/components/games-list/component.js
+++ src/components/games-list/component.js
@@ -5,15 +5,34 @@ import {
 	Input,
 	Output,
 	EventEmitter,
-	ChangeDetectionStrategy
+	ChangeDetectionStrategy,
+	trigger,
+	state,
+	transition,
+	style,
+	animate
 } from '@angular/core'
 import template from './template.html!text'
-import style from './style.css!text'
+import styles from './style.css!text'
+import {Observable, Subject, BehaviorSubject} from 'rxjs'
 
 @Component({
 	selector: 'games-list',
 	template,
-	styles: [style],
+	styles: [styles],
+	animations: [
+		trigger('search', [
+			state('hidden', style({
+				transform: 'translateX(0)'
+			})),
+			state('visible', style({
+				transform: 'translateX(calc(-100%))'
+			})),
+			transition('* <=> *', [
+				animate(150)
+			])
+		])
+	]
 	// changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class GamesList {
@@ -21,10 +40,51 @@ export default class GamesList {
 	@Input() state
 	@Output() onGameAdd = new EventEmitter()
 	@Output() onGameClick = new EventEmitter()
+	constructor() {
+		this.searchState = 'hidden'
+		this.searchInput$ = new Subject
+		this.searchQuery$ = this.searchInput$
+			.debounceTime(150)
+			.map((query = '') => query.toLowerCase())
+			.startWith()
+
+		this.gamesInput$ = new BehaviorSubject()
+		this.games$ = Observable.combineLatest(
+			this.searchQuery$,
+			this.gamesInput$,
+			(query, games) => {
+				if (!query) {
+					return games
+				} else {
+					return games.filter(game => (
+						game.title.toLowerCase().includes(query)
+					))			
+				}
+			}
+		)
+	}
 	addGame($event) {
 		this.onGameAdd.emit()
 	}
 	gameClick(game) {
 		this.onGameClick.emit(game)
+	}
+	toggleSearch(value = this.searchState) {
+		this.searchState = value === 'hidden' ? 'visible' : 'hidden'
+	}
+	ngOnChanges(changes) {
+		if (changes.games) {
+			this.gamesInput$.next(changes.games.currentValue)
+		}
+	}
+	focus(e, el) {
+		if (e.toState === 'visible') el.focus()
+		if (e.toState === 'hidden') {
+			el.value = ''
+			this.searchInput$.next()
+		}
+	}
+	doSearch(query) {
+		this.searchInput$.next(query)
 	}
 }
