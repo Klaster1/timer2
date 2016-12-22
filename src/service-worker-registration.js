ac35349301d5b2f95596032e9bab199e30cdd436
Klaster_1 <Klaster1@gmail.com>
2016-12-22 23:16:38 +0700

    add serviceworker

---
+++ src/service-worker-registration.js
@@ -0,0 +1,48 @@
+if ('serviceWorker' in navigator) {
+	if (Notification && Notification.permission !== 'granted') {
+		Notification.requestPermission()
+	}
+
+	const notify = ({
+		title = 'Timer says',
+		icon = 'src/favicon.ico',
+		body,
+		onclick = () => {}
+	}) => {
+		if (Notification && Notification.permission === 'granted') {
+			Object.assign(new Notification(title, {
+				icon,
+				body,
+			}), {onclick})
+		}
+		console.debug(body)
+	}
+
+	navigator.serviceWorker.register('service-worker.js')
+	.then(reg => {
+		reg.onupdatefound = () => {
+			const installingWorker = reg.installing
+
+			installingWorker.onstatechange = () => {
+				switch (installingWorker.state) {
+					case 'installed':
+						if (navigator.serviceWorker.controller) {
+							notify({
+								body: 'New or updated content is available.'
+							})
+						} else {
+							notify({body: 'Content is now available offline!'})
+						}
+						break
+					case 'redundant':
+						notify({body: 'The installing service worker became redundant.'})
+						break
+				}
+			}
+		}
+	})
+	.catch((e) => {
+		notify({body: 'Error during service worker registration.'})
+		console.error(e)
+	})
+}
---
+++ sw.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	cacheId: 'timer',
+	staticFileGlobs: [
+		'src/*.bundle.js',
+		'src/**/*.svg',
+		'index.html',
+		'src/*.ico',
+		'jspm_packages/system.js',
+		'jspm.config.js',
+		'jspm_packages/npm/systemjs-plugin-babel@0.0.16.json',
+		'jspm_packages/npm/material-design-icons-iconfont@3.0.2/dist/fonts/MaterialIcons-Regular.woff2'
+	],
+	maximumFileSizeToCacheInBytes: 10485760,
+	navigateFallback: '/index.html'
+}
