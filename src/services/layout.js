e52be7b1bf15939fee1342995890c710974ee8f7
Klaster_1 <Klaster1@gmail.com>
2016-12-26 22:22:57 +0700

    run layout service event callback in the zone

--- src/services/layout.js
+++ src/services/layout.js
@@ -1,13 +1,13 @@
-import {Injectable} from '@angular/core'
+import {Injectable, NgZone} from '@angular/core'
 import {Observable} from 'rxjs'
 
 @Injectable()
 export class LayoutService {
-	constructor() {
+	constructor(zone: NgZone) {
 		this.layout$ = Observable.create(subscription => {
 			const detectLayout = matches => matches ? 'small' : 'big'
 			const mq = window.matchMedia('(max-width: 960px)')
-			const fn = e => subscription.next(detectLayout(e.matches))
+			const fn = e => zone.run(() => subscription.next(detectLayout(e.matches)))
 			mq.addListener(fn)
 			subscription.next(detectLayout(mq.matches))
 			return () => mq.removeListener(fn)
